894ba514f1722668e6a99049325d290795f74595
Bernadette McKnight <sissyhanks@yahoo.com>
2021-07-27 17:55:49 -0400

    define product tag columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,18 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    id: {
+    id:{
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
     },
-    
-  },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
