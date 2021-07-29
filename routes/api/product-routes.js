8fe30140ccfbda602618a7d3a0b9fde8ff4527e5
Bernadette McKnight <sissyhanks@yahoo.com>
2021-07-29 13:02:02 -0400

    get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findAll({
+      include: [{model: Tag}, {model: Category}],
+    });
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+// router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+//   // be sure to include its associated Category and Tag data
+//   try{
+//     const 
+//   }
+// });
 
 // create new product
 router.post('/', (req, res) => {
