ae9bcca25144a7a90674b868150aacf7262de6fd
Bernadette McKnight <sissyhanks@yahoo.com>
2021-07-29 20:55:24 -0400

    update a tag's name

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updateTag[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(updateTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
