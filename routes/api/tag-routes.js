f07b5bee51f6bfeec86526aafb72ca7c650e36e2
Bernadette McKnight <sissyhanks@yahoo.com>
2021-07-29 21:02:23 -0400

    delete one tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,8 +64,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteTag) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(deleteCategory)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
